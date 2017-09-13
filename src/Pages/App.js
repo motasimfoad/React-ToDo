305f67673c7546a640972c3966dce34729af375f
Motasim Foad <charchill@Motasims-MacPro.local>
2017-09-13 20:16:27 +0600

    Adding sass

---
+++ src/Pages/App.js
@@ -0,0 +1,23 @@
+import React, {
+   Component
+ } from 'react';
+
+import { Button } from 'react-bootstrap';
+
+import './styleApp.scss';
+
+class App extends Component {
+
+
+
+  render(){
+
+    return (
+    <div className="ws">
+    Hi
+      </div>
+    );
+  }
+}
+
+export default App;
